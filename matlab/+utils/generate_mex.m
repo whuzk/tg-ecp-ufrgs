21284cbf9c18f72042efee8c4abaf4ee9a9a341a
Diego Sogari <diego.sogari@gmail.com>
2014-06-02 14:46:42 -0300

    compilacao dos novos arquivos em c

--- matlab/+utils/generate_mex.m
+++ matlab/+utils/generate_mex.m
@@ -2,12 +2,6 @@ blaslib = fullfile(matlabroot, 'extern', 'lib', computer('arch'), 'microsoft', '
 
 mex 'ccode\c_preprocess_filter.c';
 mex 'ccode\c_preprocess_detect.c';
-mex 'ccode\c_pang_jpoints.c';
-mex 'ccode\c_mohebbi_ijpoints.c';
-mex 'ccode\c_mohebbi_stdiff.c';
-mex 'ccode\c_mohebbi_segments.c';
-mex 'ccode\c_rocha_stdev.c';
-mex 'ccode\c_rocha_segments.c';
-mex 'ccode\c_gopalak_segments.c';
-mex('-largeArrayDims', 'ccode\c_rocha_hermite.c', blaslib);
-mex('-largeArrayDims', 'ccode\c_gopalak_hermite.c', blaslib);
+mex 'ccode\c_mohebbi_features.c';
+mex('-largeArrayDims', 'ccode\c_rocha_features.c', blaslib);
+mex('-largeArrayDims', 'ccode\c_gopalak_features.c', blaslib);
