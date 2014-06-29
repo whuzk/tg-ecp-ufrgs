9a4504c4243a036c0f80a2203704ed12fc5b941a
Diego Sogari <diego.sogari@gmail.com>
2014-06-29 16:10:41 -0300

    rotina para produzir estatisticas coletivas

--- matlab/+utils/generate_col_stats.m
+++ matlab/+utils/generate_col_stats.m
@@ -3,13 +3,12 @@ function tables = generate_col_stats(basedir)
 tables = cell(20,1);
 for i = 1:5
     disp(['Getting statistics for configuration ' num2str(i)]);
-    load([basedir '\networks' num2str(i) '.mat']);
+    load([basedir 'networks' num2str(i) '.mat']);
     tables{4*(i-1)+1} = get_stat_table(RochaNet);
     tables{4*(i-1)+2} = get_stat_table(MohebbiNet);
     tables{4*(i-1)+3} = get_stat_table(GopalakSTNet);
     tables{4*(i-1)+4} = get_stat_table(GopalakTNet);
 end
-save('matfiles\colstats.mat', 'tables');
 
 function Result = get_stat_table(networks)
 global measures
@@ -23,5 +22,6 @@ N = length(fields);
 stats = zeros(N,6);
 names = cell(N,1);
 for i = 1:length(fields)
-    stats(i,:) = networks.(fields(i)).stat;
+    stats(i,:) = networks.(fields{i}).stat;
+    names{i} = fields{i};
 end
